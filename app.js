39c1ac9e1c8c9fba9ec0bc6acd86aca9c4a16734
Matt Cowles <selwocm@gmail.com>
2022-10-19 16:50:31 -0700

    connect db and create home route with server port

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const express = requrie("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/action-item");
+}
+main().then(() => console.log("Connected to yelp-camp db"));
+main().catch((err) => console.log("AN ERROR!", err));
+
+app.get("/team", async (req, res) => {
+  res.send("Hello");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
