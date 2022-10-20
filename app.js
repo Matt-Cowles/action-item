cfbc3a17f4397100ab5dfd8d533ea5aba893d14c
Matt Cowles <selwocm@gmail.com>
2022-10-19 20:56:49 -0700

    add boilerplate to layouts

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/action-item");
 }
-main().then(() => console.log("Connected to yelp-camp db"));
+main().then(() => console.log("Connected to db"));
 main().catch((err) => console.log("AN ERROR!", err));
 
-app.get("/team", async (req, res) => {
-  res.send("Hello");
-});
-
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/team", async (req, res) => {
+  res.render("team");
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
