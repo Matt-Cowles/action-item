c8986ed27f1454aed01ca5fc1c180f9658772bae
Matt Cowles <selwocm@gmail.com>
2022-10-25 11:29:42 -0700

    seed the database with list of employees

--- seeds/employees.js
+++ seeds/employees.js
@@ -29,6 +29,6 @@ module.exports = [
   },
   {
     name: "Jazmin Batiste",
-    positon: "MFG Associate III",
+    position: "MFG Associate III",
   },
 ];
--- seeds/index.js
+++ seeds/index.js
@@ -3,20 +3,21 @@ const Employee = require("../models/employee");
 const mongoose = require("mongoose");
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/yelp-camp");
+  await mongoose.connect("mongodb://localhost:27017/action-item");
 }
 main().then((data) => console.log("Connected"));
 main().catch((err) => console.log("AN ERROR!", err));
 
 const seedDB = async () => {
-  await Employee.deleteMany();
-  for (let i = 0; i < 9; i++) {
-    const employee = await Employee({
+  await Employee.deleteMany({});
+  for (let i = 0; i < employees.length; i++) {
+    const employee = new Employee({
       name: `${employees[i].name}`,
       position: `${employees[i].position}`,
     });
+    console.log(`${employees[i].name}`);
+    await employee.save();
   }
-  await employee.bulkSave();
 };
 
 seedDB().then(() => mongoose.connection.close());
