3e2baa0e9a81559113d26a8071125b045cce3cbf
Matt Cowles <selwocm@gmail.com>
2022-10-29 17:18:47 -0700

    find items from db in /team/:id route

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.post("/employee", async (req, res) => {
 });
 
 app.get("/team/:id", async (req, res) => {
-  const employee = await Employee.findById(req.params.id, {});
-  // const items = await Item.findById({ owner: req.params.id });
-  res.render("employee", { employee });
+  const employee = await Employee.findById(req.params.id);
+  const items = await Item.findById(req.params.id);
+  res.render("employee", { employee, items });
 });
 
 app.get("/team/:id/edit", async (req, res) => {
